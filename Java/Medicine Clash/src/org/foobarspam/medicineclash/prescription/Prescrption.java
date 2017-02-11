d457f6c2851a267af15aa9eeb9e7a93927397865
garciaalberto <Bertooooo10@gmail.com>
2017-02-11 20:51:16 +0100

    esqueleto

---
+++ Java/Medicine Clash/src/org/foobarspam/medicineclash/prescription/Prescrption.java
@@ -0,0 +1,16 @@
+package org.foobarspam.medicineclash.prescription;
+
+
+import java.util.Date;
+
+public class Prescription {
+    
+    private Date dispenseDate = new Date();
+    private int daysSupply = 30;
+    
+    public Prescription(Date dispenseDate, int daysSupply) {
+        this.dispenseDate = dispenseDate;
+        this.daysSupply = daysSupply;
+    }
+
+}
